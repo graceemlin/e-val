d9afb0de03b265190891259eedbcbfa4e1bee6b4
shadonra <shadonra@gmail.com>
2024-01-03 21:27:46 -0800

    add full flag

--- e-val.py
+++ e-val.py
@@ -17,8 +17,9 @@ if __name__ == "__main__":
     argparser.add_argument("-o", "--outDir", help="Directory to write generated sgf files to", default=".")
     argparser.add_argument("-v", "--anki", help="Output the variations as an anki deck", default=False, action='store_true')
     argparser.add_argument("-t", "--truncate", help="Truncate output sgf, so that the only moves in it are the variation", default=False, action='store_true')
+    argparser.add_argument("-f", "--full-game", help="analyse whole game", default=False, action='store_true')
     argparser.add_argument("sgf_file", help="SGF file to analyze")
-    argparser.add_argument("move", type=int, nargs='+', help="One or more move numbers to analyse, note that in handicap games the handicap stones might not be considered a move")
+    argparser.add_argument("move", type=int, nargs='*', help="One or more move numbers to analyse, note that in handicap games the handicap stones might not be considered a move")
 
     args = argparser.parse_args()
     try:
@@ -30,6 +31,8 @@ if __name__ == "__main__":
     rules = args.rules
     game = Game(metadata,stones,moves,rules)
     engine = AnalysisEngine(args.katago,args.analysis_config, args.model)
+    if args.full_game:
+        args.move = range(len(game.moves))[2:-1]
     for m in tqdm.tqdm(args.move):
         query = AnalysisQuery(sgf_basename+"_q"+str(m)+".sgf", game, board, args.sgf_file)
         query.analyseTurn(m)
